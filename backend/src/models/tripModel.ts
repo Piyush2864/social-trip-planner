b2475ba352047f7b0cd0b841573fe32ecf73a4ff
Piyush Choudhary <piyush.choudhary6264@gmail.com>
2025-05-15 13:06:28 +0530

    trip model and types define

---
+++ backend/src/models/tripModel.ts
@@ -0,0 +1,45 @@
+import mongoose, { Schema } from 'mongoose';
+import { Trip } from '../types/tripType';
+
+
+const tripSchema = new Schema<Trip>({
+    title: {
+        type: String,
+        required: true
+    },
+
+    destination: {
+        type: String,
+        required: true
+    },
+
+    startDate: {
+        type: Date,
+    },
+
+    endDate: {
+        type: Date
+    },
+
+    description: {
+        type: String
+    },
+
+    creator: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    },
+
+    members: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }],
+    
+    tags: {
+        type: [String]
+    }
+}, {timestamps: true});
+
+const Trip = mongoose.model('Trip', tripSchema);
+
+export default Trip;
---
+++ backend/src/types/tripType.ts
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+import {Document} from 'mongoose';
+
+export interface Trip extends Document {
+    _id: string;
+    title: string;
+    destination: string;
+    startDate: Date;
+    endDate: Date;
+    description: string;
+    creator?: mongoose.Types.ObjectId | string;
+    members:  mongoose.Types.ObjectId |string[];
+    tags: string[];
+}
